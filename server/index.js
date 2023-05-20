4f7959bc08ecb0c89aee40cceba0fb955e1dd9a2
David Coulson <theinvisiblebench2@gmail.com>
2023-05-20 11:00:30 -0500

    testconfig

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '',
+  user: '',
+  password: '',
+  database: ''
+});
+
+
+app.get('/coolman', (req, res) => {
+  res.send('{"name":"David", "lastname":"Coulson"}')
+});
 
 app.listen(port, () => {
   console.log(`Server started! Listening on port ${port}`)
-})
+});
