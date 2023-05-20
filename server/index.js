0bbfc303e5eea99169afdf0e945b754b925b7813
David Coulson <theinvisiblebench2@gmail.com>
2023-05-20 14:40:30 -0500

    backend stuff

--- server/index.js
+++ server/index.js
@@ -4,17 +4,43 @@ const port = 3000;
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: '',
-  user: '',
-  password: '',
-  database: ''
+  host: 'localhost',
+  user: 'ants',
+  password: '1234567890',
+  database: 'password_schmischmi_com'
 });
 
-
-app.get('/coolman', (req, res) => {
-  res.send('{"name":"David", "lastname":"Coulson"}')
+//Connect to database
+connection.connect(function (err) {
+  if (err) console.log("Could not connect to database");
+  else console.log(`Successfully connected to database ${connection.database} with user ${connection.user}`);
 });
 
+//Starts the express server
 app.listen(port, () => {
   console.log(`Server started! Listening on port ${port}`)
+});
+
+app.get('/coolman', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ user: 'David', password: 'Coulson' }));
+});
+
+//Account Login
+app.get('/login/:userID/:passwordEnc', (req, res) => {
+  var user = req.params['userID'];
+  var passowrd = req.params['passwordEnc'];
+  console.log(req.params);
+  //SQL
+  var sqlquery = `INSERT INTO accounts (username, password) VALUES ('${userID}', '${password}');`;
+  connection.query(sqlquery, function (err, result){
+    console.log(result);
+  });
+});
+
+//Account Sign-up
+app.get('/register/:userID/:passwordEnc', (req, res) => {
+  var user = req.params['userID'];
+  var passowrd = req.params['passwordEnc'];
+  console.log(req.params);
 });
